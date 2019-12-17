809b3958cac123ca5316f2c0cbc2b83d162643f8
Aaron Salazar <aaron.salazar@capmation.com>
2019-12-16 22:07:53 -0500

    Added MITM working

--- MiTM.py
+++ MiTM.py
@@ -4,10 +4,11 @@ from random import randint
 import tkinter as tk
 from des import DesKey
 
+
 LARGE_FONT = ("Verdana", 12)
 bindsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 bindsocket.setblocking(1)
-bindsocket.bind(('', 8081))
+bindsocket.bind(('', 9000))
 bindsocket.listen(5)
 fromaddr = None
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,10 +19,7 @@ priv_key = randint(1, q)
 #  AXA mod q
 pub_key = (alpha ** priv_key) % q
 dh_key = 0
-print("pub_key", pub_key)
-shared_key = None
-
-
+socket_list = []
 
 class ChatApp(tk.Tk):
 
@@ -60,8 +58,7 @@ class StartPage(tk.Frame):
     # usuario inserta su nombre y presiona conectar para realizar la conexion con el servidor
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        # self.bind("<<ShowFrame>>", self.on_show_frame)
-        # self.waitForConnection()
+
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
         self.messageBox = tk.Text()
@@ -69,38 +66,35 @@ class StartPage(tk.Frame):
         sendMessageText = tk.Entry()
         sendMessageText.pack()
 
-        sendButton = tk.Button(self, text="Send",
-                            command=lambda: self.send_to_chat(sendMessageText.get()))
-        sendButton.pack()
+        sendToConn1 = tk.Button(self, text="Send To Connection 1",
+                                command=lambda: self.send_to_1(sendMessageText.get()))
+        sendToConn1.pack()
 
-        connectButton = tk.Button(self, text="Connect", command=lambda: [self.waiting_thread(), self.connect()])
+        sendToConn2 = tk.Button(self, text="Send To Connection 2",
+                                command=lambda: self.send_to_2(sendMessageText.get()))
+        sendToConn2.pack()
 
-        connectButton.pack()
+        self.waiting_thread()
+
+        # connectButton = tk.Button(self, text="Connect", command=lambda: [self.waiting_thread(), self.connect()])
+        #
+        # connectButton.pack()
 
 
-    def send_to_chat(self, message):
+
+    def send_to_1(self, message):
         global s, shared_key
         print(message + " sent")
         to_send = message.encode('utf-8')
         to_send = shared_key.encrypt(to_send, padding=True)
-        s.sendall(to_send)
+        socket_list[0].sendall(to_send)
 
-    def connect(self):
-        global s, key_exchange, pub_key, dh_key, shared_key
-        s.connect(('', 9000))
-        s.sendall(bytes(str(pub_key), 'utf8'))
-        key_exchange = s.recv(1024)
-        strings = str(key_exchange, 'utf8')
-        num = int(strings)
-        dh_key = (num ** priv_key) % q
-        str_temp = "00000" + str(dh_key)
-        print(str_temp)
-        shared_key = DesKey(str_temp.encode('utf-8'))
-        print("sent Pub Key", pub_key)
-        t = threading.Thread(target=self.recieve, args=[s])
-        t.start()
-
-
+    def send_to_2(self, message):
+        global s, shared_key
+        print(message + " sent")
+        to_send = message.encode('utf-8')
+        to_send = shared_key.encrypt(to_send, padding=True)
+        socket_list[1].sendall(to_send)
 
     def recieve(self, newsocket):
         print("DH_key:", dh_key)
@@ -119,14 +113,14 @@ class StartPage(tk.Frame):
         t.start()
 
     def waitForConnection(self):
-        global bindsocket, s, key_exchange, pub_key, dh_key, shared_key
+        global bindsocket, s, key_exchange, pub_key, dh_key, shared_key, socket_list
 
         print("waiting for connection")
-        # if __name__ == '__main__':
 
         while True:
             try:
                 (newsocket, fromaddr) = bindsocket.accept()
+                socket_list.append(newsocket)
                 print("Connected")
                 while True:
                     key_exchange = None
@@ -135,10 +129,11 @@ class StartPage(tk.Frame):
                         strings = str(key_exchange, 'utf8')
                         num = int(strings)
                         dh_key = (num ** priv_key) % q
-                        str_temp = "000000" + str(dh_key)
+                        str_temp = "00000" + str(dh_key)
                         print(str_temp)
                         shared_key = DesKey(str_temp.encode('utf-8'))
-                        break
+                        newsocket.sendall(bytes(str(pub_key), 'utf8'))
+
                     break
                 print("DH_ Exchange", num)
 
@@ -152,39 +147,5 @@ class StartPage(tk.Frame):
         print("closing")
         bindsocket.close()
 
-
-    def deal_with_client(conn, addr):
-        data = None
-        text = ''
-        print('Chatting with: ' + str(addr))
-        while text != 'disconnect':
-            while data is None:
-                data = conn.recv(1024)
-
-            print("Client says: " + data.decode('ASCII'))
-
-            if data.decode('ASCII') != 'disconnect':
-                data = None
-            else:
-                break
-
-            text = input('Server: ')
-            conn.sendall(text.encode('ASCII'))
-        conn.close()
-
-
-
-
 app = ChatApp()
 app.mainloop()
-
-
-
-# read bloqueante
-# def recieveMessage():
-
-
-
-
-
-
